82def91aca4a04f7dbfc64f9f4bd7853185ebacd
gsalaman <43499190+gsalaman@users.noreply.github.com>
2018-10-04 14:43:10 -0600

    Comment updates

--- timer_cb.cpp
+++ timer_cb.cpp
@@ -1,4 +1,23 @@
-/* FILE HEADER GOES HERE!!! */
+/*====================================================
+ * TIMER_CB.CPP
+ * 
+ * This library provides a timer callback service.  
+ * 
+ *  First iteration only allows for one timer outstanding
+ *  at a given point in time.
+ *
+ *  Implementation:
+ *     Since timers on the arduino only go to a couple seconds,
+ *     we create a tick timer with associated ISR that gets 
+ *     called every 10 ms.  We use that to 
+ *     check and see whether a given timer has expired.
+ *   
+ * Side Effects and Dependencies:
+ *    Takes over TIMER1.   
+ *======================================================*/
+
+#include "timer_cb.h"
+#include <arduino.h>
 
 #if !defined(TRUE)
 #define TRUE 1
@@ -8,11 +27,27 @@
 #define FALSE 0
 #endif
 
+// No explicit init function needed; we'll use this 
+// flag to check and see if we need to init the library.
 static bool timer_cb_inited=FALSE;
-void (*user_cb)(void);
+
+// Currently only support one timer at a given time.
+void (*user_cb)(void)=NULL;
+
+// uptime mark:  when does our timer expire.
 unsigned long expire_time;
 
-/* NEED FUNCTION HEADER */
+/*====================================================
+ * INTERNAL Function:  init_tick_timer
+ * 
+ * Description:
+ *  This function initializes our timer module, specifically
+ *  setting up TIMER1 as a 10ms periodic timer.
+ *
+ * Parameters:  None
+ *
+ * Return Value: None
+ *======================================================*/
 static void init_tick_timer( void )
 {
   // current iteration:  tick every 10 ms.
@@ -34,27 +69,55 @@ static void init_tick_timer( void )
 
 }  // init_tick_timer
 
-/* NEED FUNCTION HEADER */
+/*====================================================
+ * API Function:  timer_cb_reg
+ * 
+ * Description:
+ *  This function registers a callback function to fire
+ *  at a given time in the future.
+ * 
+ * Parameters:
+ *    cb_func:  function to call after specified time.  
+ *              Note no return value or parameters.
+ *    expire_ms:  how many MS in the future to call the 
+ *                callback function
+ *
+ * Return Value: error code for registering the timer.  
+ *               See timer_cb_reg_return_type above for specifics.
+ * 
+ * Side Effects and Dependencies:
+ *    callback function will run in interrupt space, so make it as
+ *    lightweight as possible.
+ *======================================================*/
 timer_cb_reg_return_type timer_cb_reg(void (*cb_func)(void), int expire_ms)
 {
   unsigned long current_time;
   
+  // Check to see whether we've initialized the timer callback module.
   if (!timer_cb_inited)
   {
     init_tick_timer();  
   }
 
+  // what time is it now?
   current_time = millis();
+
+  // how far in the future will our timer expire?
   expire_time = current_time + expire_ms;
+
+  // set the user callback.
   user_cb = cb_func;
   
-}
+}  // timer_cb_reg
 
 /*====================================================================
  * Time tick ISR
  *
  * This is the interrupt service routine that the arduino calls whenever our
- * 10ms timer expires.  
+ * 10ms timer expires.
+ *
+ * We currently check current time and, if the expire for our callback has passed,
+ * call the callback function.   
  * ==================================================================*/
 ISR(TIMER1_COMPA_vect)
 {
@@ -70,4 +133,4 @@ ISR(TIMER1_COMPA_vect)
     user_cb();
   }
 
-}
+}  // Time Tick ISR
